d5a70269947ec2d72577c421e539e2f26c744ce8
EnderMythex <102812268+EnderMythex@users.noreply.github.com>
2024-02-13 15:36:20 +0100

    Update main.py
    UPDATE 2

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
 import discord
 from discord.ext import commands
+import datetime
+import atexit
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("[SUCCES  ] ONLINE ") # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+    channel = bot.get_channel(1234567890) # ------------------------------------- EDIT THIS LINE : Replace 1234567890 with your channel ID (Required) -------------------------------------
+    embed = discord.Embed(
+        title='🟢 Bot Online', # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+        description='[V2.2.3] ONLINE', # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+        color=discord.Color.green()
+    )
+    await channel.send(embed=embed)
+
+@bot.command()
+async def stop(ctx):
+    if ctx.message.channel.id == 1234567890:  # ------------------------------------- EDIT THIS LINE : Replace 1234567890 with your channel ID (Required) -------------------------------------
+        embed = discord.Embed(
+            title='🔴 Bot Offline', # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+            description='[V2.2.3] STOPED', # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+            color=discord.Color.red()
+        )
+        await ctx.send(embed=embed)
+        await bot.close()
+    else:
+        await ctx.send('You cannot use this command in this channel.') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
-        return  
+        return
 
-    if message.channel.name == 'CHANNEL':
-        role = discord.utils.get(message.guild.roles, name="ROLE NAME") # modify it
+    if message.channel.name == 'CHANNEL NAME': # ------------------------------------- EDIT THIS LINE (Required) -------------------------------------
+        role = discord.utils.get(message.guild.roles, name="ROLE NAME") # ------------------------------------- EDIT THIS LINE (Required) -------------------------------------
         if role:
-            await message.channel.send(f'{role.mention} MESSAGE') # modify it
-            await message.add_reaction('👍') 
-            await message.add_reaction('❤')
-            print(f"Message envoyé dans le channel : {message.channel.name}")
+            reply = await message.reply(f'{role.mention} Successfully pinged ! ') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+            await message.add_reaction('👍') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+            await message.add_reaction('❤') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+            print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] [SUCCES  ] Successfully pinged in : {message.channel.name} {message.guild.name}") # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
         else:
-            await message.channel.send('Rôle non trouvé.')
+            await message.channel.send('Role not found.') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
 
-    await bot.process_commands(message)
+    if message.channel.name == 'CHANNEL NAME': # ------------------------------------- EDIT THIS LINE (Required) -------------------------------------
+        role = discord.utils.get(message.guild.roles, name="ROLE NAME") # ------------------------------------- EDIT THIS LINE (Required) -------------------------------------
+        if role:
+            reply = await message.reply(f'{role.mention} Successfully pinged ! ') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+            await message.add_reaction('👍') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+            await message.add_reaction('❤') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+            print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] [SUCCES  ] Successfully pinged in : {message.channel.name} {message.guild.name}") # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
+        else:
+            await message.channel.send('Role not found.') # ------------------------------------- EDIT THIS LINE (Not required) -------------------------------------
 
-bot.run('BOT TOKEN HERE') # modify it
+atexit.register(bot.close)
+bot.run('BOT TOKEN HERE') # ------------------------------------- EDIT THIS LINE (Required) -------------------------------------
